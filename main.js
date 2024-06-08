489ac1c994c9523dc7749a7c0af44ac5cb6003bd
Rayhaan Perager <rxp843@cs.bham.ac.uk>
2024-06-08 15:08:35 +0100

    Fixed scrolling issue with quick links

--- main.js
+++ main.js
@@ -1,117 +1,19 @@
-// Scrolls to Home section
-document.addEventListener('DOMContentLoaded', function () {
-    var portfolioButton = document.querySelector('[href="#"]');
+//Quick Scroll Links
+document.addEventListener("DOMContentLoaded", () => {
+  var link = document.querySelectorAll('a[href^="#"]');
 
-    portfolioButton.addEventListener('click', function (event) {
-        event.preventDefault();
+  link.forEach((link) => {
+    link.addEventListener("click", (event) => {
+      event.preventDefault();
 
-        var portfolioSection = document.getElementById('#');
-        var offset = portfolioSection.offsetTop;
-        window.scrollTo({ top: offset, behavior: 'smooth' });
+      //Remove the "#" from the href
+      var targetId = link.getAttribute("href").substring(1);
+      var targetSection = document.getElementById(targetId);
+
+      if (targetSection) {
+        var offset = targetSection.offsetTop;
+        window.scrollTo({ top: offset, behavior: "smooth" });
+      }
     });
-});
-
-// Scrolls to Prayer Times section
-document.addEventListener('DOMContentLoaded', function () {
-    var portfolioButton = document.querySelector('[href="#prayer-times"]');
-
-    portfolioButton.addEventListener('click', function (event) {
-        event.preventDefault();
-
-        var portfolioSection = document.getElementById('prayer-times');
-        var offset = portfolioSection.offsetTop;
-        window.scrollTo({ top: offset, behavior: 'smooth' });
-    });
-});
-
-// Scrolls to Prayer Times section
-document.addEventListener('DOMContentLoaded', function () {
-    var portfolioButton = document.querySelector('[href="#prayer-times2"]');
-
-    portfolioButton.addEventListener('click', function (event) {
-        event.preventDefault();
-
-        var portfolioSection = document.getElementById('prayer-times');
-        var offset = portfolioSection.offsetTop;
-        window.scrollTo({ top: offset, behavior: 'smooth' });
-    });
-});
-
-
-// Scrolls to About Us section
-document.addEventListener('DOMContentLoaded', function () {
-    var portfolioButton = document.querySelector('[href="#about-us"]');
-
-    portfolioButton.addEventListener('click', function (event) {
-        event.preventDefault();
-
-        var portfolioSection = document.getElementById('about-us');
-        var offset = portfolioSection.offsetTop;
-        window.scrollTo({ top: offset, behavior: 'smooth' });
-    });
-});
-
-// Scrolls to About Us section
-document.addEventListener('DOMContentLoaded', function () {
-    var portfolioButton = document.querySelector('[href="#about-us2"]');
-
-    portfolioButton.addEventListener('click', function (event) {
-        event.preventDefault();
-
-        var portfolioSection = document.getElementById('contact-us');
-        var offset = portfolioSection.offsetTop;
-        window.scrollTo({ top: offset, behavior: 'smooth' });
-    });
-});
-
-// Scrolls to Donate section
-document.addEventListener('DOMContentLoaded', function () {
-    var portfolioButton = document.querySelector('[href="#donate"]');
-
-    portfolioButton.addEventListener('click', function (event) {
-        event.preventDefault();
-
-        var portfolioSection = document.getElementById('donate');
-        var offset = portfolioSection.offsetTop;
-        window.scrollTo({ top: offset, behavior: 'smooth' });
-    });
-});
-
-// Scrolls to Donate section
-document.addEventListener('DOMContentLoaded', function () {
-    var portfolioButton = document.querySelector('[href="#donate2"]');
-
-    portfolioButton.addEventListener('click', function (event) {
-        event.preventDefault();
-
-        var portfolioSection = document.getElementById('donate');
-        var offset = portfolioSection.offsetTop;
-        window.scrollTo({ top: offset, behavior: 'smooth' });
-    });
-});
-
-// Scrolls to Contact Us section
-document.addEventListener('DOMContentLoaded', function () {
-    var portfolioButton = document.querySelector('[href="#contact-us"]');
-
-    portfolioButton.addEventListener('click', function (event) {
-        event.preventDefault();
-
-        var portfolioSection = document.getElementById('contact-us');
-        var offset = portfolioSection.offsetTop;
-        window.scrollTo({ top: offset, behavior: 'smooth' });
-    });
-});
-
-// Scrolls to Contact Us section
-document.addEventListener('DOMContentLoaded', function () {
-    var portfolioButton = document.querySelector('[href="#contact-us2"]');
-
-    portfolioButton.addEventListener('click', function (event) {
-        event.preventDefault();
-
-        var portfolioSection = document.getElementById('contact-us');
-        var offset = portfolioSection.offsetTop;
-        window.scrollTo({ top: offset, behavior: 'smooth' });
-    });
+  });
 });

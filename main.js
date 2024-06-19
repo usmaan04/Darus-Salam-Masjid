130009a5f5a9584abc82fc06d54d2360dea451d1
usmaan04 <usmaanahmed2004@gmail.com>
2024-06-20 00:56:27 +0100

    fully functional responsive navbar

--- main.js
+++ main.js
@@ -49,6 +49,17 @@ function setupNavbarLinkActivation() {
   });
 }
 
+// Toggle the navigation menu for mobile view
+document.addEventListener('DOMContentLoaded', function() {
+  const hamburger = document.querySelector('.hamburger');
+  const nav = document.querySelector('nav');
+
+  hamburger.addEventListener('click', function() {
+      hamburger.classList.toggle('active');
+      nav.classList.toggle('active');
+  });
+});
+
 // Setup Currency field in Donate Section
 function setupCurrencyInputField() {
   const currencyInput = document.querySelector("input[data-type='currency']");
